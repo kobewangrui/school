2248075f523cd3c7d7847ba3ae9dad55b09d6e39
wangruilong <862452159@qq.com>
2017-08-15 13:50:09 +0800

    8.14内容提交

--- src/views/content/index.vue
+++ src/views/content/index.vue
@@ -1,5 +1,50 @@
-<!-- <style src="assets/css/index.css" scoped></style> -->
+ <style src="assets/css/index.css" scoped></style> 
 <template>
-	<div>index</div>
+	<div>
+		<header>
+			<ul>
+				<router-link tag="li" to="/">
+					<p>每日之星</p>
+					<p> 
+						<span></span>
+					</p> 
+				</router-link>
+				<router-link tag="li" to="/">
+					<p>优秀学生</p>
+					<p>
+						<span></span>
+					</p>
+				</router-link>
+				<router-link tag="li" to="/">
+					<p>辅导员之声</p>
+					<p>
+						<span></span>
+					</p>
+				</router-link>
+				<router-link tag="li" to="/">
+					<p>公告</p>
+					<p>
+						<span></span>
+					</p>
+				</router-link>
+			</ul>
+			<div class="line"></div>
+		</header>
+		<section>
+			<div v-for="i in 3" class="noticeGroup">
+				<p class="title">国庆放假通知</p>
+				<p class="date">
+					<span class="top">置顶</span>
+					<span class="time">2017-09-28 09:30</span>
+				</p>
+				<p class="content">国庆放假通知国庆放假通知国庆放假通知国庆放假通知国庆放假通知国庆放假通知国庆放假通知国庆放假通知</p>
+				<p class="img">
+					<img v-if="i===1" :src="require('assets/image/timg.jpg')">
+					<img v-if="i===2 || i===1" :src="require('assets/image/timg.jpg')">
+					<img v-if="i==3 || i===2 || i===1" :src="require('assets/image/timg.jpg')">
+				</p>
+			</div>
+		</section>
+	</div>
 </template>
-<!-- <script src="assets/js/index.js"></script> -->
+ <script src="assets/js/index.js"></script> 
--- src/views/frame.vue
+++ src/views/frame.vue
@@ -1,5 +1,20 @@
+<style lang="css">
+	*{
+		padding:0;
+		margin:0;
+		font-family:"Microsoft Yahei","微软雅黑",'华文细黑',Tahoma,Arial,Helvetica,STHeiti;
+	}
+	body{
+		color: #333;
+		padding: 0 .3rem;
+		box-sizing: border-box;
+	}
+	ul li{
+		list-style: none;
+	}
+</style>
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+	<div>
+		<router-view></router-view>
+	</div>
 </template>
